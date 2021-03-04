9b401877513d9fa0d2606cce4966e005665682a6
mario muuk <62722269+DrSuperMario@users.noreply.github.com>
2021-03-04 18:22:01 +0200

    added collector

--- util/collector.py
+++ util/collector.py
@@ -2,34 +2,39 @@ import requests
 from bs4 import BeautifulSoup as BS4
 from textwrap import dedent
 import re
-import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import (
+                        create_engine, 
+                        insert, 
+                        update, 
+                        MetaData, 
+                        Table, 
+                        String, 
+                        Column, 
+                        Integer
+                )
 
 
 html_list = list()
+list_of_products = list()
+list_of_prices = list()
 dict_of_products = dict()
 
-    
 def master():
 
-    
     req = requests.get('https://www.selver.ee/')
     soup = BS4(req.content, features='html.parser')
-
     #esilehelt URL otsingu regex
     url_class = re.compile(r'(item level0 nav.\w+.+\d)')
     #Hinna ja toote otsingu regex
-    product_class = re.compile(r'(col-lg-3 \w+).+')
-
+    product_class = re.compile(r'(col-lg-3 \w+).+') 
+    price_and_qty = re.compile(r'\s+(\d+.+)')
     #Leia kõik HTML Li tagilt mis hoiab URLi
-    find_tag = soup.find_all('li', {'class': url_class})
-
+    find_tag = soup.find_all('li',{'class':url_class})
     #leia URL'id ja lista toote nimekirja pikkus
     get_urls = [i.find_next('a').get('href')+"/?limit=96&p=" for i in find_tag]
 
+
     #start of the urls and end of the URLS
-
-
     for z in range(5,23):
         #Find product last page
         product_html = requests.get(get_urls[z])
@@ -52,25 +57,33 @@ def master():
             find_product_group_titles = [i.find_next('a').get('title') for i in find_product_group]
             prices = [dedent(i.text.replace(u'\xa0',' ')) for i in find_product_group_prices if len(i) > 1]
             for t,f in zip(find_product_group_titles,prices):
-                dict_of_products.update({t:re.split(r'\s+([0-9]+.[0-9]+\s+./[A-z]+)',f)[:2]})
-                
-        
-    df = pd.DataFrame(index=dict_of_products.keys(),data=[y for _, y in dict_of_products.items()], columns=['Hind','Hind/Kogus)'])
-    df.reset_index(inplace=True)
-    df['ID'] = range(len(dict_of_products))
-    df.to_sql('Selver_items', table_c, if_exists='replace')
+                dict_of_products.update({t:re.split(price_and_qty,f)[:2]})
+    
+    return dict_of_products 
 
 
 if(__name__=="__main__"):
 
-    table = create_engine("sqlite:///selver_data.db", echo=True)
-    table_c = table.connect()
+    table_conn = create_engine('sqlite:///selver_data.db', echo=True)
 
-    master()
+    dict_of_producrs = master()
 
-    with table_c.connect() as c:
-        c.execute('ALTER TABLE `Selver_items` ADD PRIMARY KEY (`ID`);')
+    toode = [{'Toode':x,'Hind':y[0],'Hind/Kogus':y[1]} for x,y in dict_of_products.items()]
 
-    
-    table_c.close()
+
+    with table_conn.connect() as conn:
+
+        meta = MetaData(table_conn)
+        table = Table(
+                        'Selver', meta,
+                        Column('id', Integer, primary_key=True, autoincrement=True),
+                        Column('Toode', String),
+                        Column('Hind', String),
+                        Column('Hind/Kogus', String),
+                 )
+
+        meta.create_all()
+
+        conn.execute(table.insert(None),toode)
+        
 
