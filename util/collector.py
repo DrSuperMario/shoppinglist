d9f50df03c1dccb72852150baf13102a3676fb81
mario muuk <62722269+DrSuperMario@users.noreply.github.com>
2021-03-05 19:07:21 +0200

    Commited 25

--- util/collector.py
+++ util/collector.py
@@ -1,3 +1,4 @@
+from typing import Sequence, Any
 import requests 
 from bs4 import BeautifulSoup as BS4
 from textwrap import dedent
@@ -13,63 +14,80 @@ from sqlalchemy import (
                         Integer
                 )
 
+from dataclasses import dataclass
 
-html_list = list()
-list_of_products = list()
-list_of_prices = list()
-dict_of_products = dict()
+@dataclass
+class Collect:
 
-def master():
+    html_list: list()
+    list_of_products: list()
+    list_of_prices: list()
+    dict_of_products: dict()
 
-    req = requests.get('https://www.selver.ee/')
-    soup = BS4(req.content, features='html.parser')
-    #esilehelt URL otsingu regex
-    url_class = re.compile(r'(item level0 nav.\w+.+\d)')
-    #Hinna ja toote otsingu regex
-    product_class = re.compile(r'(col-lg-3 \w+).+') 
-    price_and_qty = re.compile(r'\s+(\d+.+)')
-    #Leia kõik HTML Li tagilt mis hoiab URLi
-    find_tag = soup.find_all('li',{'class':url_class})
-    #leia URL'id ja lista toote nimekirja pikkus
-    get_urls = [i.find_next('a').get('href')+"/?limit=96&p=" for i in find_tag]
+    def __init__(self, 
+                    db_conn: str=None, 
+                    url: str = 'https://www.selver.ee/',
+                    *args,
+                    **kwargs
+                ) -> None:
 
+        self._db_conn = db_conn
+        self._url = url
+        self.url_class = re.compile(r'(item level0 nav.\w+.+\d)')
+        self.product_class = re.compile(r'(col-lg-3 \w+).+') 
+        self.price_and_qty = re.compile(r'\s+(\d+.+)')
 
-    #start of the urls and end of the URLS
-    for z in range(5,23):
-        #Find product last page
-        product_html = requests.get(get_urls[z])
-        #puhasta ilusa seebiga
-        soup = BS4(product_html.content, features='html.parser')
-        try:
-            find_product_last_page = soup.find('a',{'class':'last'}).text
-        except AttributeError:
-            find_product_last_page = 3
-        
-        for i in range(1,int(find_product_last_page)+1):
-            product_html = requests.get(get_urls[z]+str(i))
-            soup = BS4(product_html.content, features='html.parser')
-            html_list.append(soup)  
-            print(f"Page URL: {get_urls[z]+str(i)} Last page is: {find_product_last_page}")
+        self.html_list = list()
+        self.list_of_products = list()
+        self.list_of_prices = list()
+        self.dict_of_products = dict()
 
-        for y in range(len(html_list)):  
-            find_product_group = html_list[y].find_all('li',{'class':product_class})
-            find_product_group_prices = html_list[y].find_all('span',{'class':'price'})
-            find_product_group_titles = [i.find_next('a').get('title') for i in find_product_group]
-            prices = [dedent(i.text.replace(u'\xa0',' ')) for i in find_product_group_prices if len(i) > 1]
-            for t,f in zip(find_product_group_titles,prices):
-                dict_of_products.update({t:re.split(price_and_qty,f)[:2]})
     
-    return dict_of_products 
+    def collect_url(self) -> Sequence[Any]:
 
+        req = requests.get(self._url)
+        soup = BS4(req.content, features='html.parser')
+        find_tag = soup.find_all('li',{'class':self.url_class})
+        #leia URL'id ja lista toote nimekirja pikkus
+        get_urls = [i.find_next('a').get('href')+"/?limit=96&p=" for i in find_tag]
 
-if(__name__=="__main__"):
+        return get_urls
+
+    def page_data(self):
+
+        get_urls = self.collect_url()
+       
+        for z in range(5,23):
+            #Find product last page
+            product_html = requests.get(get_urls[z])
+            #puhasta ilusa seebiga
+            soup = BS4(product_html.content, features='html.parser')
+            try:
+                find_product_last_page = soup.find('a',{'class':'last'}).text
+            except AttributeError:
+                find_product_last_page = 3
+            
+            for i in range(1,int(find_product_last_page)+1):
+                product_html = requests.get(get_urls[z]+str(i))
+                soup = BS4(product_html.content, features='html.parser')
+                self.html_list.append(soup)  
+                print(f"Page URL: {get_urls[z]+str(i)} Last page is: {find_product_last_page}")
+
+            for y in range(len(self.html_list)):  
+                find_product_group = self.html_list[y].find_all('li',{'class':self.product_class})
+                find_product_group_prices = self.html_list[y].find_all('span',{'class':'price'})
+                find_product_group_titles = [i.find_next('a').get('title') for i in find_product_group]
+                prices = [dedent(i.text.replace(u'\xa0',' ')) for i in find_product_group_prices if len(i) > 1]
+                for t,f in zip(find_product_group_titles,prices):
+                    self.dict_of_products.update({t:re.split(self.price_and_qty,f)[:2]})
+
+        return [{'toode':x,'hind':y[0],'hind_kogus':y[1]} for x,y in self.dict_of_products.items()]
+
+def main():
 
     table_conn = create_engine('sqlite:///selver_data.db', echo=True)
 
-    dict_of_producrs = master()
-
-    toode = [{'Toode':x,'Hind':y[0],'Hind/Kogus':y[1]} for x,y in dict_of_products.items()]
-
+    toode = Collect().page_data()
 
     with table_conn.connect() as conn:
 
@@ -77,13 +95,16 @@ if(__name__=="__main__"):
         table = Table(
                         'Selver', meta,
                         Column('id', Integer, primary_key=True, autoincrement=True),
-                        Column('Toode', String),
-                        Column('Hind', String),
-                        Column('Hind/Kogus', String),
-                 )
+                        Column('toode', String),
+                        Column('hind', String),
+                        Column('hind_kogus', String),
+                    )
 
         meta.create_all()
 
         conn.execute(table.insert(None),toode)
-        
+       
+
+if(__name__=="__main__"):
+    main()
 
